d981399f6908b64e86547775b64b7491e0ed4ad0
Sabrina Siqueira dos Santos <ss6215245@gmail.com>
2025-10-26 13:24:15 -0300

    alteração_faq: cores, animações e manipulação

---
+++ codigo/public/app.js
@@ -0,0 +1,29 @@
+   document.addEventListener('DOMContentLoaded', () => {
+        fetch('db.json') 
+        .then(response => response.json())
+        .then(data => { 
+            for(let i=0; i<data.faqs.length; i++){
+                let faq = data.faqs[i];
+                const span = document.getElementById(`${faq.id}`);
+                if (span) {
+                span.textContent = faq.pergunta;
+                }
+                //sincronizar respostas
+                const answer = document.querySelector(`.resposta[data-id="${faq.id}"]`);
+                if(answer){
+                    answer.textContent = faq.resposta;
+                }
+            }
+            })
+              .catch(error => console.error('Erro ao carregar o arquivo JSON:', error));
+});
+
+
+        const accordions = document.querySelectorAll('.accordion');
+        accordions.forEach(accordion => {
+            accordion.addEventListener('click', () => {
+               const body= accordion.querySelector('.accordion-body');
+               //tirar e colocar active ao clicar
+               body.classList.toggle('active');
+            });
+        });
