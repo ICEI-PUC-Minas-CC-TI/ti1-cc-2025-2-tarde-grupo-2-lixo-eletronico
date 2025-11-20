1c6ed6a68062a5a020157128151eb0f2054de24c
samuel <samuelteodoro058@gmail.com>
2025-11-20 08:57:29 -0300

    feat: implementa funções de atualização e exclusão de produtos

--- codigo/public/assets/js/app_produtos.js
+++ codigo/public/assets/js/app_produtos.js
@@ -15,7 +15,7 @@ function readProduto(processaDados){
     .catch(error => {
         console.error('Erro ao ler produtos:', error);
         displayMessage('Erro ao ler produtos', 'danger');
-    })
+    });
 }
 
 //create
@@ -33,5 +33,40 @@ function createProduto(produto, refreshFunction){
     .catch(error => {
         console.error('Erro:', error);
         displayMessage('Erro ao inserir produto', 'danger');
+    });
+}
+
+//update
+function createProduto(produto, refreshFunction){
+    fetch(apiUrl,{
+        method:'PUT',
+        headers: {'Content-Type':'application/json'},
+        body:JSON.stringify(produto),
     })
+    .then(response => response.json())
+    .then(data => {
+        displayMessage("Produto atualizado com sucesso!");
+        if(refreshFunction) refreshFunction();
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+        displayMessage('Erro ao atualizar o produto', 'danger');
+    });
+}
+
+//delete
+function deleteProduto(id, refreshFunction){
+    fetch(`${apiUrl}/${id}`, {
+        method: 'DELETE',
+    })
+    .then(response => {
+        if(response.ok) {
+        displayMessage("Produto removido com sucesso!");
+        if(refreshFunction) refreshFunction();
+    }
+    })
+    .catch(error => {
+        console.error('Erro', error);
+        displayMesssage('Erro ao remover produto', 'danger');
+    });
 }
